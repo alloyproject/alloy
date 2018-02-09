ab427e5c6f7cf031325a79c60920bbaa24d9f36d
nighthawk <nighthawk18@protonmail.com>
2018-02-09 09:29:02 -0800

    version updates

--- external/rocksdb/util/build_version.cc
+++ external/rocksdb/util/build_version.cc
@@ -1,4 +1,4 @@
-#include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:1637e12ec8a30d378597b97c585f3340ace446b3";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:2018-02-08";
-const char* rocksdb_build_compile_date = __DATE__;
+#include "build_version.h" 
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:0"; 
+const char* rocksdb_build_git_datetime = "rocksdb_build_git_datetime:02/09/18 09:12:01"; 
+const char* rocksdb_build_compile_date = __DATE__; 
