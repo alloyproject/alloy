b510f4d7f4e2ad524194eb7981cbed158cd27972
nighthawk <nighthawk18@protonmail.com>
2018-02-09 09:46:21 -0800

    fixes for versioning in cmake

--- external/rocksdb/util/build_version.cc
+++
@@ -1,4 +0,0 @@
-#include "build_version.h" 
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:0"; 
-const char* rocksdb_build_git_datetime = "rocksdb_build_git_datetime:02/09/18 09:12:01"; 
-const char* rocksdb_build_compile_date = __DATE__; 
