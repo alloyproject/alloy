06e456981d5d9d6e9b0a7dfa2cfbdde5ebb1d2f4
nighthawk18 <nighthawk18@protonmail.com>
2018-02-23 17:43:39 -0800

    SW improvements, rpc fixes, TS fixes

--- src/CryptoNoteCore/BlockchainCache.cpp
+++ src/CryptoNoteCore/BlockchainCache.cpp
@@ -534,17 +534,25 @@ uint32_t BlockchainCache::getTimestampLowerBoundBlockIndex(uint64_t timestamp) c
     return 0;
   }
 
-  uint32_t blockIndex = parent->getTimestampLowerBoundBlockIndex(timestamp);
-  return blockIndex == INVALID_BLOCK_INDEX ? blockIndex : startIndex;
 
-//From the Turtlecoin logic suggestion
-//https://github.com/turtlecoin/turtlecoin/commit/1af3f4e2400fc4eec5afb921a6d8c7be25fa7a29
-// try {
-//     return parent->getTimestampLowerBoundBlockIndex(timestamp);
-//   } catch (std::runtime_error&) {
-//     // parent didn't have the block, so index.front() must be the block we're looking for
-//     return startIndex;
-//   }
+//Handle the getTimestampLowerBoundBlockIndex error situation. Credit to turtle coin and deertacos for suggestion.
+
+ try {
+
+  uint32_t blockIndex = parent->getTimestampLowerBoundBlockIndex(timestamp);
+return blockIndex == INVALID_BLOCK_INDEX ? blockIndex : startIndex;
+
+}
+ catch (std::runtime_error&) {
+  logger(Logging::DEBUGGING) << "Encountered Rare getTimestampLowerBoundBlockIndex() error.";
+
+     // parent didn't have the block, so index.front() must be the block we're looking for
+     return startIndex;
+   }
+
+
+
+
 
 }
 
