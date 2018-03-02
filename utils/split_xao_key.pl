c90576311632eff7904791f28e480be70fb37c6d
nighthawk18 <nighthawk18@protonmail.com>
2018-03-02 10:09:38 -0800

    splitter fix

--- utils/split_xao_key.pl
+++ utils/split_xao_key.pl
@@ -2,11 +2,9 @@
 # Usage: split_xao_key.pl <LONG_KEY>
 #@MongBoy
 
-print ("You didn't provide a GUI Wallet key to split. Use --help for command info.") && exit 1 if (!$tempStr);
-if ($tempStr eq '--help') {
-        print ("Usage: split_xao_key.pl <LONG_KEY>");
-        exit 0;
-}
+$tempStr=$ARGV[0];
+
+
 my @keys = split / /,(join ' ', unpack '(A64)*', $tempStr);
 print ("\nPublic Spend Key: ",$keys[0]);
 print ("Public View keys: ",$keys[1]);
