196d024ab9ff1e42ac9c6e8495dd026888b844e2
nighthawk <nighthawk18@protonmail.com>
2018-03-06 16:39:28 -0800

    win build improvements

--- winbuild.cmd
+++ winbuild.cmd
@@ -1,9 +1,28 @@
 @echo off
+
 mkdir build
 del /s /q build
 cd build
-"C:\Program Files\CMake\bin\cmake.exe" -G  "Visual Studio 14 2015 Win64"  ..
 
+if "%1"=="dyn" goto :dyn
+echo Static Build
+cmake.exe -G  "Visual Studio 14 2015 Win64"  ..
 msbuild Alloy.sln /p:Configuration=Release
 
+
+EXIT /B 
+
+
+
+:dyn
+echo Dynamic Build
+cmake.exe -G  "Visual Studio 14 2015 Win64"  -DSTATIC=NO  ..
+msbuild Alloy.sln /p:Configuration=Release
+
+echo Copying libs
+del /s /q \opt\alloy\libs
+md \opt\alloy\libs
+for /r "\opt\alloy" %%x in (*.lib) do copy "%%x" "\opt\alloy\libs"
+
+ 
 
